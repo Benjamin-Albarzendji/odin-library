6e6d7494cea71e0f878829d50bb1416fadc70639
Benjamin-Albarzendji <benjamin.albarzendji@gmail.com>
2022-10-30 22:36:47 +0100

    Formatting

--- script.js
+++ script.js
@@ -1,158 +1,129 @@
-let check = 0
+let check = 0;
 
 //The Library for the books
-let myLibrary = []
-
-
-
+let myLibrary = [];
 
 //The class for the Book objects
 class Book {
-    constructor(title, author, readStatus, pages) {
-        this.title = title
-        this.author = author
-        this.readStatus = readStatus
-        this.pages = pages
-    }
+  constructor(title, author, readStatus, pages) {
+    this.title = title;
+    this.author = author;
+    this.readStatus = readStatus;
+    this.pages = pages;
+  }
 }
 
 //Placeholder books
-const gameOfThrones = new Book("A Game of Thrones", "George R.R Martin", "Book Read", "720")
-const hyperion = new Book("Hyperion", "Dan Simmons", "Book Read", "482")
-
+const gameOfThrones = new Book(
+  "A Game of Thrones",
+  "George R.R Martin",
+  "Book Read",
+  "720"
+);
+const hyperion = new Book("Hyperion", "Dan Simmons", "Book Read", "482");
 
 //Pushing the books to the library
-myLibrary.push(gameOfThrones)
-myLibrary.push(hyperion)
+myLibrary.push(gameOfThrones);
+myLibrary.push(hyperion);
 
-
-pullInformation()
-pullInformation()
-
+pullInformation();
+pullInformation();
 
 function pullInformation() {
+  let title = myLibrary[check].title;
+  let author = myLibrary[check].author;
+  let readStatus = myLibrary[check].readStatus;
+  let pages = myLibrary[check].pages;
 
-    let title = myLibrary[check].title
-    let author = myLibrary[check].author
-    let readStatus = myLibrary[check].readStatus
-    let pages = myLibrary[check].pages
-
-    bookAdd(title, author, readStatus, pages)
+  bookAdd(title, author, readStatus, pages);
 }
 
 function bookAdd(title, author, readStatus, pages) {
-    //Creates the book structure 
-    let book = document.createElement("div")
-    book.className = "card"
-    book.dataset.id = check
-    const titleDiv = document.createElement("div")
-    const authorDiv = document.createElement("div")
-    const readDiv = document.createElement("div")
-    const pagesDiv = document.createElement("div")
-    const button = document.createElement("button")
+  //Creates the book structure
+  let book = document.createElement("div");
+  book.className = "card";
+  book.dataset.id = check;
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const readDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const button = document.createElement("button");
 
-    //Appends Children to main div
-    book.appendChild(titleDiv)
-    book.appendChild(authorDiv)
-    book.appendChild(readDiv)
-    book.appendChild(pagesDiv)
-    book.appendChild(button)
+  //Appends Children to main div
+  book.appendChild(titleDiv);
+  book.appendChild(authorDiv);
+  book.appendChild(readDiv);
+  book.appendChild(pagesDiv);
+  book.appendChild(button);
 
+  //Information provided
+  titleDiv.innerText = `${title}`;
+  authorDiv.innerText = `${author}`;
+  readDiv.innerText = `${readStatus}`;
+  pagesDiv.innerText = `${pages} pages`;
+  button.innerText = "Delete";
 
-    //Information provided
-    titleDiv.innerText = `${title}`
-    authorDiv.innerText = `${author}`
-    readDiv.innerText = `${readStatus}`
-    pagesDiv.innerText = `${pages} pages`
-    button.innerText = "Delete"
+  //Eventlistener for button
+  button.addEventListener("click", (e) => {
+    //Gets the data-id from the parent, removes it from the website and removes it from the array
+    const parent = e.target.parentNode;
+    console.log((indexPosition = parent.getAttribute("data-id")));
+    myLibrary.splice(indexPosition, 1);
+    parent.remove();
+  });
 
-
-    //Eventlistener for button
-    button.addEventListener("click", (e) => {
-
-        //Gets the data-id from the parent, removes it from the website and removes it from the array
-        const parent = e.target.parentNode
-        console.log(indexPosition = parent.getAttribute("data-id"))
-        myLibrary.splice(indexPosition, 1)
-        parent.remove()
-    })
-
-    //Global index value that gets incremeneted wth every book
-    check += 1
-    //Adds it to the cardContainer structure
-    let cardContainer = document.querySelector(".cardsContainer")
-    cardContainer.appendChild(book)
-
+  //Global index value that gets incremeneted wth every book
+  check += 1;
+  //Adds it to the cardContainer structure
+  let cardContainer = document.querySelector(".cardsContainer");
+  cardContainer.appendChild(book);
 }
 
-
 function showForm() {
-    document.getElementById("form").classList.add("show")
-    document.getElementById("pop").classList.add("show")
-    window.addEventListener("click", windowOnClick);
-
+  document.getElementById("form").classList.add("show");
+  document.getElementById("pop").classList.add("show");
+  window.addEventListener("click", windowOnClick);
 }
 
 function addForm() {
+  document.getElementById("form").addEventListener("click", function (event) {
+    event.preventDefault();
+  });
 
-    document.getElementById("form").addEventListener("click", function (event) {
-        event.preventDefault()
-    });
+  const form = document.getElementById("form");
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  let read = document.querySelector("#read");
 
-    const form = document.getElementById("form")
-    const title = document.querySelector("#title")
-    const author = document.querySelector("#author")
-    const pages = document.querySelector("#pages")
-    let read = document.querySelector("#read")
+  if (title.value === "") {
+    return;
+  } else if (author.value === "") {
+    return;
+  } else if (pages.value === "") {
+    return;
+  }
 
-    if (title.value === "") {
-        return
-    }
-    else if (author.value === "") {
-        return
-    }
+  if (read.checked === true) {
+    read = "Book Read";
+  } else {
+    read = "Not Read";
+  }
 
-    else if (pages.value === "") {
-        return
-    }
+  const newBook = new Book(title.value, author.value, read, pages.value);
+  myLibrary.push(newBook);
+  pullInformation();
+  title.value = "";
+  author.value = "";
+  pages.value = "";
 
-
-    if (read.checked === true) {
-        read = "Book Read"
-    }
-    else {
-        read = "Not Read"
-    }
-
-    const newBook = new Book(title.value, author.value, read, pages.value)
-    myLibrary.push(newBook)
-    pullInformation()
-    title.value = ""
-    author.value = ""
-    pages.value = ""
-
-
-
-    document.getElementById("form").classList.remove("show")
-    document.getElementById("pop").classList.remove("show")
-
-
-
+  document.getElementById("form").classList.remove("show");
+  document.getElementById("pop").classList.remove("show");
 }
-
 
 function windowOnClick(e) {
-
-    if (e.target === document.getElementById("pop")) {
-        document.getElementById("form").classList.remove("show")
-        document.getElementById("pop").classList.remove("show")
-    }
+  if (e.target === document.getElementById("pop")) {
+    document.getElementById("form").classList.remove("show");
+    document.getElementById("pop").classList.remove("show");
+  }
 }
-
-
-
-
-
-
-
-
